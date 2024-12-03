55860e214b5502eb7232bdddd068fda8a2270fd3
jsnytte <snyttejason@gmail.com>
2024-12-03 17:29:57 -0500

    algo1_heap implement and test

--- Algorithm1.h
+++ Algorithm1.h
@@ -1,4 +1,14 @@
+#ifndef ALGORITHM1_H
+#define ALGORITHM1_H
+#include <vector>
+#include "Movie.h"
+
+using namespace std;
+
 class Algorithm1
 {
-
+public:
+    static Movie findKthRatedMovieHeap(vector<Movie>& movies, int k);
 };
+
+#endif
