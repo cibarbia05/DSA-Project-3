9414961fb2943e327401198dd88d7a25f80111c1
jsnytte <snyttejason@gmail.com>
2024-12-02 19:45:23 -0500

    input handling

---
+++ input_helpers.h
@@ -0,0 +1,14 @@
+//
+// Created by Jason Snytte on 12/2/24.
+//
+
+#ifndef INPUT_HELPERS_H
+#define INPUT_HELPERS_H
+
+#include <string>
+
+int promptGenre();
+int promptDecade();
+int promptAge();
+
+#endif //INPUT_HELPERS_H
