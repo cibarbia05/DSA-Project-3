3fd0c909fc058cc230a0f2fdf11af278fa5d76ab
Christian Ibarbia RUiz <christianibarbia2015@gmail.com>
2024-12-04 16:38:52 -0500

    Used chrono to track algorithm time and fixed spacing for printed statements

--- DataHandling/UserInput.cpp
+++ DataHandling/UserInput.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include "UserInput.h"
 #include "vector"
 #include "set"
@@ -17,14 +18,14 @@ std::vector<std::string> askUserForGenres() {
     std::string input;
     bool keepAsking = true;
 
-    std::cout << "Available genres:\n";
+    std::cout << "Available genres:" << std::endl;
     for (size_t i = 1; i < GENRE_NAMES.size(); ++i) {
         std::cout << i << ": " << GENRE_NAMES[i] << "\n";
     }
-    std::cout << "Enter the number corresponding to the genre you DO NOT like. Enter 0 if you don't dislike any more genres.\n";
+    std::cout << "Enter the number corresponding to the genre you DO NOT like. Enter 0 if you don't dislike any more genres." << std::endl;
 
     while (keepAsking) {
-        std::cout << "Your choice: ";
+        std::cout << "Your choice: " << std::endl;
         std::cin >> input;
 
         try {
@@ -38,17 +39,17 @@ std::vector<std::string> askUserForGenres() {
                 }
             } else if (genreIndex > 0 && static_cast<size_t>(genreIndex) < GENRE_NAMES.size()) {
                 if (selectedIndices.find(genreIndex) != selectedIndices.end()) {
-                    std::cout << "You have already added \"" << GENRE_NAMES[genreIndex] << "\" to your list. Choose another genre.\n";
+                    std::cout << "You have already added \"" << GENRE_NAMES[genreIndex] << "\" to your list. Choose another genre." << std::endl;
                 } else {
                     likedGenres.push_back(GENRE_NAMES[genreIndex]);
                     selectedIndices.insert(genreIndex);
                     std::cout << "Added \"" << GENRE_NAMES[genreIndex] << "\" to your dislike list.\n";
                 }
             } else {
-                std::cout << "Invalid choice. Please choose a number between 1 and " << GENRE_NAMES.size() - 1 << ", or 0 to stop.\n";
+                std::cout << "Invalid choice. Please choose a number between 1 and " << GENRE_NAMES.size() - 1 << ", or 0 to stop." << std::endl;
             }
         } catch (const std::invalid_argument&) {
-            std::cout << "Invalid input. Please enter a valid number.\n";
+            std::cout << "Invalid input. Please enter a valid number." << std::endl;
         }
     }
     return likedGenres;
@@ -88,7 +89,7 @@ int getAgeInput() {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
-        std::cout << " Invalid age! Please enter a number between 0 and 120: ";
+        std::cout << " Invalid age! Please enter a number between 0 and 120:" << std::endl;
     }
 }
 
@@ -113,7 +114,7 @@ int getNumInput() {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
-        std::cout << " Invalid number! Please enter a number between 1 and 1644: ";
+        std::cout << " Invalid number! Please enter a number between 1 and 1644:" << std::endl;
     }
 }
 
@@ -130,7 +131,7 @@ std::string getYesNoInput() {
         if (validateYesNo(input)) {
             return toLowercase(input);
         }
-        std::cout << " Invalid input! Please enter (Y)es or (N)o: ";
+        std::cout << " Invalid input! Please enter (Y)es or (N)o:" << std::endl;
     }
 }
 
@@ -146,7 +147,7 @@ void displayWelcomeArt() {
 int userInput() {
     // Display welcome art
     displayWelcomeArt();
-    std::cout << "Welcome to Film Finders! \nWe are dedicated to helping you find your best fit movie reccomendation.\nFirst lets get to know you!" << std::endl;
+    std::cout << "Welcome to Film Finders! \nWe are dedicated to helping you find your best fit movie recommendation.\nFirst lets get to know you!" << std::endl;
 
     // Variables to store user responses
     std::string name;
@@ -156,52 +157,60 @@ int userInput() {
     int option;
 
     // Introduction Question
-    std::cout << "> What's your name? ";
+    std::cout << "> What's your name?" << std::endl;
     std::getline(std::cin, name);
 
     //name not empty
     while (name.empty() || name.find_first_not_of(" \t") == std::string::npos) {
-        std::cout << " Please enter a valid name: ";
+        std::cout << " Please enter a valid name:" << std::endl;
         std::getline(std::cin, name);
     }
 
-    std::cout << "\n> Hi, " << name << "! How old are you? ";
+    std::cout << "\n> Hi, " << name << "! How old are you?" << std::endl;
     age = getAgeInput();
 
-    std::cout <<"Everyone has something they can't stand, so first we gotta ask, what don't you like?"<< std::endl;
+    std::cout <<"\nEveryone has something they can't stand, so first we gotta ask, what don't you like?"<< std::endl;
     std::vector<std::string> userGenres = askUserForGenres();
-    std::cout << "> Thanks for the information " << name << "! We'll take this into consideration." << std::endl;
+    std::cout << "\n> Thanks for the information " << name << "! We'll take this into consideration." << std::endl;
     // Second Yes/No Question
-    std::cout << "\n> Now, Pick a number that represents how adventurous you're feeling today.\nThe lower the number, the more predictable your recommendation might be. ";
+    std::cout << "\n> Now, Pick a number that represents how adventurous you're feeling today.\nThe lower the number, the more predictable your recommendation might be." << std::endl;
     int kth_choice = getNumInput();
 
     // Option Selection Question
     std::cout << "\n> Please choose your preferred option:" << std::endl;
     std::cout << "   1. Algo 1" << std::endl;
     std::cout << "   2. Algo 2" << std::endl;
-    std::cout << "Enter your choice (1 or 2): ";
+    std::cout << "Enter your choice (1 or 2): " << std::endl;
 
     while (!(std::cin >> option) || (option != 1 && option != 2)) {
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << " Invalid input! Please enter 1 or 2: ";
+        std::cout << "Invalid input! Please enter 1 or 2:" << std::endl;
     }
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     vector<Movie> movies = parseData();
     int k = kth_choice;
 
-    std::cout << "\n Film Finders Results:" << std::endl;
+    std::cout << "\nFilm Finders Results:" << std::endl;
     std::cout << "___________________________________________________________________________________________" << std::endl;
-    std::cout << "   "<< name << "'s " << "Top movie reccomendation!"<< std::endl;
+    std::cout << "   "<< name << "'s " << "Top movie recommendation!"<< std::endl;
+
+    // Track algorithm duration
+    auto start = std::chrono::high_resolution_clock::now();
+
     if (option == 1) {
         Movie kthLargestRatingMovieHeap = Algorithm1::findKthRatedGenre(movies, k, userGenres);
-        cout << "\n" <<"      "<< kthLargestRatingMovieHeap.getName() << " - " << kthLargestRatingMovieHeap.getDate() << "\n" << "       Rating: " << kthLargestRatingMovieHeap.getAvgRating()<< " - Genre: " <<kthLargestRatingMovieHeap.getGenre();
+        cout << "\n" <<"     "<< kthLargestRatingMovieHeap.getName() << " - " << kthLargestRatingMovieHeap.getDate() << "\n" << "       Rating: " << kthLargestRatingMovieHeap.getAvgRating()<< " - Genre: " <<kthLargestRatingMovieHeap.getGenre();
     } else {
         Movie kthLargestRatingMovie = Algorithm2::findKthHighestWithGenre(movies, k, userGenres);
-        cout <<"     "<< kthLargestRatingMovie.getName() << " - " << kthLargestRatingMovie.getDate() << "\n" << "     Rating: "<< kthLargestRatingMovie.getAvgRating() << " - Genre: " <<kthLargestRatingMovie.getGenre();
+        cout <<"     "<< kthLargestRatingMovie.getName() << " - " << kthLargestRatingMovie.getDate() << "\n" << "     Rating: "<< kthLargestRatingMovie.getAvgRating() << " - Genre: " <<kthLargestRatingMovie.getGenre() << std::endl;
     }
-    std::cout << "   " << std::endl;
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> duration = end - start;
+
     std::cout << "___________________________________________________________________________________________" << "\n" << std::endl;
+    std::cout << "The algorithm you chose took " << duration.count() << " seconds to execute" << std::endl;
     std::cout << "Thank you for using Film Finders! Happy watching." << std::endl;
     return 0;
 }
